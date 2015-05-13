c3add6aff5a6cc5c9af7553e4be92bdfa84cb3b5
tania rosas-jimenez <rosastania@yahoo.com>
2015-05-12 17:59:44 -0700

    video 11

--- index.php
+++ index.php
@@ -37,6 +37,19 @@ function getUserID($userName){
 	echo $results['data']['0']['id'];
 }
 
+//Function to print images onto screen
+function printImages($userID){
+	$url = 'http://api.instagram.com/v1/users/'.$userID.'/media/recent?client_id='.clientID.'&count=5';
+	$instagramInfo = connectToInstagram($url);
+	$results = json_decode($instagramInfo, true);
+	//Parse through the information one by one
+	foreach ($results['data'] as $items) {
+		$image_url = $items['images']['low_resolution']['url'];//going to go through all of my results and give myself back the URL of those pictures 
+		//because we want to have it in the PHP Server.
+		echo '<img src=" '.$image_url.'"/>br/>';
+	}
+}
+
 if (isset($_GET['code'])){
 	$code = ($_GET['code']);
 	$url = 'https://api.instagram.com/oauth/access_token';
@@ -57,7 +70,11 @@ $result = curl_exec($curl);
 curl_close($curl);
 
 $results = json_decode($result, true);
-getUserID($result['user']['username']);
+$userName =  $results['user']['username'];
+
+$userID = getUserID($username);
+
+printImages($userID);
 }
 else{
 
